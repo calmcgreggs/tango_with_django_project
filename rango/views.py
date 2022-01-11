52e13f688cfe842d9caf9df8e7de6fb8d055a28a
Calum McGregor <2518508m@student.gla.ac.uk>
2022-01-11 13:15:30 +0000

    Changes after Chapter 3 Completion

---
+++ rango/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<a href='/rango/about/'>Rango says hey there partner!</a>")
+
+def about(request):
+    return HttpResponse("<a href='/rango/'>Rango says here is the about page.</a>")
